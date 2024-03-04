90f6b730e5990b962638a21bbb69f6d2da1ece53
jglee96 <zakklee96@gmail.com>
2024-03-04 19:38:51 +0900

    add: snap 구현
    * openlayer snap interaction 참고
    [링크](https://openlayers.org/en/latest/examples/snap.html)
    * rbush 사용하여 r-tree 자료구조 생성
    * rbush에 검색되는 polygon 중에서 가장 가까운 polygon에서 가장 가까운 점
    계산

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,142 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import RBush from "rbush";
+import * as jsts from "jsts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const NAVER_MAP_API = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${
+  import.meta.env.VITE_OAPI_KEY
+}&submodules=geocoder,drawing`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+type TreeObject = {
+  minX: number;
+  minY: number;
+  maxX: number;
+  maxY: number;
+  value: naver.maps.Polygon;
+};
+
+let script: HTMLScriptElement | null = document.querySelector(
+  `script[src="${NAVER_MAP_API}"]`
+);
+
+if (script === null) {
+  script = document.createElement("script");
+  script.src = NAVER_MAP_API;
+  document.head.appendChild(script);
+}
+
+script.onload = () => {
+  const tree = new RBush<TreeObject>();
+  const map = new naver.maps.Map("map", {
+    zoom: 20,
+  });
+
+  const mapDiv = map.getElement();
+
+  const addPolyBtn = document.createElement("button");
+  addPolyBtn.innerText = "Add Polygon";
+
+  const debugRect: naver.maps.Rectangle[] = [];
+  const factory = new jsts.geom.GeometryFactory();
+  addPolyBtn.onclick = () => {
+    const path: naver.maps.LatLng[] = [];
+    const poly = new naver.maps.Polygon({
+      map,
+      paths: [path],
+    });
+    const handleClick = map.addListener("click", (e) => {
+      console.log("click");
+      path.push(e.latlng);
+      if (path.length === 1) {
+        path.push(e.latlng);
+      }
+      poly.setPath(path);
+    });
+
+    const handleMove = map.addListener("mousemove", (e) => {
+      console.log("mousemove");
+      if (path.length < 1) return;
+      const p1 = (e.latlng as naver.maps.LatLng).destinationPoint(135, 5);
+      const p2 = (e.latlng as naver.maps.LatLng).destinationPoint(315, 5);
+      const bounds = new naver.maps.LatLngBounds(p1, p2);
+      const result = tree.search({
+        minX: bounds.minX(),
+        minY: bounds.minY(),
+        maxX: bounds.maxX(),
+        maxY: bounds.maxY(),
+      });
+      const jPoint = factory.createPoint(
+        new jsts.geom.Coordinate(e.latlng.x, e.latlng.y)
+      );
+
+      const comp = result.map((item: TreeObject) => {
+        const coords = (item.value.getPath() as naver.maps.KVOArrayOfCoords)
+          .getArray()
+          .reduce(
+            (acc: jsts.geom.Coordinate[], cur) => [
+              ...acc,
+              new jsts.geom.Coordinate(cur.x, cur.y),
+            ],
+            []
+          );
+        coords.push(coords[0]);
+        const jPoly = factory.createPolygon(coords);
+
+        // jsts.operation.distance.DistanceOp.nearestPoints(jPoly, jPoint);
+        return {
+          distance: jsts.operation.distance.DistanceOp.distance(jPoly, jPoint),
+          poly: jPoly,
+        };
+      });
+      if (comp.length > 0) {
+        comp.sort((a, b) => a.distance - b.distance);
+        const [p1, p2] = jsts.operation.distance.DistanceOp.nearestPoints(
+          comp[0].poly,
+          jPoint
+        );
+        const snapPoint =
+          p1.compareTo(new jsts.geom.Coordinate(e.latlng.x, e.latlng.y)) === 0
+            ? p2
+            : p1;
+
+        path.pop();
+        path.push(new naver.maps.LatLng(snapPoint.y, snapPoint.x));
+        poly.setPath(path);
+      } else {
+        path.pop();
+        path.push(e.latlng);
+        poly.setPath(path);
+      }
+
+      // jsts.operation.distance.DistanceOp.nearestPoints();
+      debugRect.forEach((rect) => rect.setMap(null));
+
+      debugRect.push(
+        ...result.map(
+          (item: TreeObject) =>
+            new naver.maps.Rectangle({
+              map,
+              bounds: item.value.getBounds(),
+            })
+        )
+      );
+    });
+
+    map.addListenerOnce("rightclick", () => {
+      console.log("rightclick");
+      naver.maps.Event.removeListener([handleClick, handleMove]);
+
+      path.pop();
+      poly.setPath(path);
+      const bounds = poly.getBounds() as naver.maps.LatLngBounds;
+      const item = {
+        minX: bounds.minX(),
+        minY: bounds.minY(),
+        maxX: bounds.maxX(),
+        maxY: bounds.maxY(),
+        value: poly,
+      };
+      tree.insert(item);
+    });
+  };
+  document.body.insertBefore(addPolyBtn, mapDiv);
+};
